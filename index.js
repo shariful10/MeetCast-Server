9db239b016854186baeb0284bbe8ee5233115986
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-08-18 12:02:31 +0600

    JWT Implemented

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+	const authorization = req.headers.authorization;
+	if (!authorization) {
+		return res.status(401).send({ error: true, message: "Invalid Token" });
+	}
+	// Bearer token
+	const token = authorization.split(" ")[1];
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+		if (err) {
+			return res.status(401).send({ error: true, message: "Invalid Token" });
+		}
+		req.decoded = decoded;
+		next();
+	});
+};
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bq2ef3t.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -28,7 +44,14 @@ const client = new MongoClient(uri, {
 async function run() {
 	try {
 		const usersCollection = client.db("meetcastDb").collection("users");
-		
+
+		// JWT Tokens
+		app.post("/jwt", (req, res) => {
+			const user = req.body;
+			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+			res.send({ token });
+		});
+
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
 		console.log("Pinged your deployment. You successfully connected to MongoDB!");
