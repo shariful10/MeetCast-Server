7f862201f33e3355c7e35a748ece6750aba56196
Pranto Sheikh <prantosheikh834@gmail.com>
2023-09-14 22:20:16 +0600

    completed payment method

--- data.js
+++ data.js
@@ -40,55 +40,152 @@
       "Mail & Calendar",
       "Essential Apps"
    ]
+},
+{
+  "title": "Free",
+  "description": "Basic plan with limited features",
+  "pricing": "$0/year",
+  "price" : 0,
+  "features": [
+    "Meetings up to 40 minutes per meeting",
+    "50 Attendees per meeting",
+    "Whiteboard Basic",
+    "Team Chat",
+    "Mail & Calendar"
+  ]
+},
+{
+  "title": "Platinum",
+  "description": "Premium plan with exclusive features",
+  "pricing": "$299.99/year",
+  "price" : 299,
+  "features": [
+    "Meetings up to 30 hours per meeting",
+    "200 Attendees per meeting",
+    "Whiteboard Basic",
+    "Team Chat",
+    "Mail & Calendar",
+    "Essential Apps"
+  ]
+},
+{
+  "title": "Business",
+  "description": "Standard plan for everyday use",
+  "pricing": "$149.99/year",
+  "price" : 149,
+  "features": [
+    "Meetings up to 30 hours per meeting",
+    "100 Attendees per meeting",
+    "Whiteboard",
+    "Mail & Calendar",
+    "Essential Apps"
+  ]
 }
 ]
 
+
+
+
+
+// aa
+
+[
+  {
+    "_id": "64f9c84ff56681301fde2533",
+    "title": "Free",
+    "description": "Basic plan with limited features",
+    "pricing": "$0/month",
+    "price": 0,
+    "features": [
+      "Meetings up to 40 minutes per meeting",
+      "50 Attendees per meeting",
+      "Whiteboard Basic",
+      "Team Chat",
+      "Mail & Calendar"
+    ]
+  },
+  {
+    "_id": "64f9c84ff56681301fde2534",
+    "title": "Platinum",
+    "description": "Premium plan with exclusive features",
+    "pricing": "$29.99/month",
+    "price": 29,
+    "features": [
+      "Meetings up to 30 hours per meeting",
+      "200 Attendees per meeting",
+      "Whiteboard Basic",
+      "Team Chat",
+      "Mail & Calendar",
+      "Essential Apps"
+    ]
+  },
+  {
+    "_id": "64f9c84ff56681301fde2535",
+    "title": "Business",
+    "description": "Standard plan for everyday use",
+    "pricing": "$14.99/month",
+    "price": 14,
+    "features": [
+      "Meetings up to 30 hours per meeting",
+      "100 Attendees per meeting",
+      "Whiteboard",
+      "Mail & Calendar",
+      "Essential Apps"
+    ]
+  },
+  
+  {
+    "_id": "64f9c895f56681301fde70c2",
+    "title": "Free",
+    "description": "Basic plan with limited features",
+    "pricing": "$0/year",
+    "price": 0,
+    "features": [
+    "Meetings up to 40 minutes per meeting",
+    "50 Attendees per meeting",
+    "Whiteboard Basic",
+    "Team Chat",
+    "Mail & Calendar"
+    ]
+    },
+    {
+    "_id": "64f9c895f56681301fde70c3",
+    "title": "Platinum",
+    "description": "Premium plan with exclusive features",
+    "pricing": "$299.99/year",
+    "price": 299,
+    "features": [
+    "Meetings up to 30 hours per meeting",
+    "200 Attendees per meeting",
+    "Whiteboard Basic",
+    "Team Chat",
+    "Mail & Calendar",
+    "Essential Apps"
+    ]
+    },
+    {
+    "_id": "64f9c895f56681301fde70c4",
+    "title": "Business",
+    "description": "Standard plan for everyday use",
+    "pricing": "$149.99/year",
+    "price": 149,
+    "features": [
+    "Meetings up to 30 hours per meeting",
+    "100 Attendees per meeting",
+    "Whiteboard",
+    "Mail & Calendar",
+    "Essential Apps"
+    ]
+    }
+]
+
+
+
+
+
+  
   
 
-    // Yearly pack 
-    
-    [
-        {
-          "title": "Free",
-          "description": "Basic plan with limited features",
-          "pricing": "$0/year",
-          "price" : 0,
-          "features": [
-            "Meetings up to 40 minutes per meeting",
-            "50 Attendees per meeting",
-            "Whiteboard Basic",
-            "Team Chat",
-            "Mail & Calendar"
-          ]
-        },
-        {
-          "title": "Platinum",
-          "description": "Premium plan with exclusive features",
-          "pricing": "$299.99/year",
-          "price" : 299,
-          "features": [
-            "Meetings up to 30 hours per meeting",
-            "200 Attendees per meeting",
-            "Whiteboard Basic",
-            "Team Chat",
-            "Mail & Calendar",
-            "Essential Apps"
-          ]
-        },
-        {
-          "title": "Business",
-          "description": "Standard plan for everyday use",
-          "pricing": "$149.99/year",
-          "price" : 149,
-          "features": [
-            "Meetings up to 30 hours per meeting",
-            "100 Attendees per meeting",
-            "Whiteboard",
-            "Mail & Calendar",
-            "Essential Apps"
-          ]
-        }
-      ]
       
 
 
--- index.js
+++ index.js
@@ -58,15 +58,21 @@ app.use(express.json());
 
 // For ZegoCLoud
 app.get("/token", (req, res) => {
-	const appID = 2059610707;
-	const serverSecret = "5692269139171731f75d087ec95f3344";
-	const userId = "user1";
-	const effectiveTimeInSeconds = 3600;
-	const payload = "";
+  const appID = 2059610707;
+  const serverSecret = "5692269139171731f75d087ec95f3344";
+  const userId = "user1";
+  const effectiveTimeInSeconds = 3600;
+  const payload = "";
 
-	const token = generateToken04(appID, userId, serverSecret, effectiveTimeInSeconds, payload);
-	console.log("Akhtar:", token);
-	res.send(token);
+  const token = generateToken04(
+    appID,
+    userId,
+    serverSecret,
+    effectiveTimeInSeconds,
+    payload
+  );
+  console.log("Akhtar:", token);
+  res.send(token);
 });
 
 // For ZegoCLoud
@@ -139,96 +145,96 @@ async function run() {
       res.send(result);
     });
 
-		app.put("/rooms/:roomId", async (req, res) => {
-			const roomId = req.params.roomId;
-			const { newName } = req.body;
+    app.put("/rooms/:roomId", async (req, res) => {
+      const roomId = req.params.roomId;
+      const { newName } = req.body;
 
-			try {
-				const updateResult = await roomsCollection.updateOne(
-					{ _id: new ObjectId(roomId) }, // Use new ObjectId()
-					{ $set: { roomName: newName } }
-				);
+      try {
+        const updateResult = await roomsCollection.updateOne(
+          { _id: new ObjectId(roomId) }, // Use new ObjectId()
+          { $set: { roomName: newName } }
+        );
 
-				if (updateResult.modifiedCount > 0) {
-					res.status(200).send("Room renamed successfully");
-				} else {
-					res.status(404).send("Room not found");
-				}
-			} catch (error) {
-				console.error("Error updating room:", error);
-				res.status(500).send("An error occurred while renaming the room");
-			}
-		});
+        if (updateResult.modifiedCount > 0) {
+          res.status(200).send("Room renamed successfully");
+        } else {
+          res.status(404).send("Room not found");
+        }
+      } catch (error) {
+        console.error("Error updating room:", error);
+        res.status(500).send("An error occurred while renaming the room");
+      }
+    });
 
-		app.get("/rooms/:email", async (req, res) => {
-			const result = await roomsCollection.find().toArray();
-			res.send(result);
-		});
+    app.get("/rooms/:email", async (req, res) => {
+      const result = await roomsCollection.find().toArray();
+      res.send(result);
+    });
 
-		app.put("/rooms/:roomId", async (req, res) => {
-			const roomId = req.params.roomId;
-			const { newName } = req.body;
+    app.put("/rooms/:roomId", async (req, res) => {
+      const roomId = req.params.roomId;
+      const { newName } = req.body;
 
-			try {
-				const updateResult = await roomsCollection.updateOne(
-					{ _id: new ObjectId(roomId) }, // Use new ObjectId()
-					{ $set: { roomName: newName } }
-				);
+      try {
+        const updateResult = await roomsCollection.updateOne(
+          { _id: new ObjectId(roomId) }, // Use new ObjectId()
+          { $set: { roomName: newName } }
+        );
 
-				if (updateResult.modifiedCount > 0) {
-					res.status(200).send("Room renamed successfully");
-				} else {
-					res.status(404).send("Room not found");
-				}
-			} catch (error) {
-				console.error("Error updating room:", error);
-				res.status(500).send("An error occurred while renaming the room");
-			}
-		});
+        if (updateResult.modifiedCount > 0) {
+          res.status(200).send("Room renamed successfully");
+        } else {
+          res.status(404).send("Room not found");
+        }
+      } catch (error) {
+        console.error("Error updating room:", error);
+        res.status(500).send("An error occurred while renaming the room");
+      }
+    });
 
-		app.get("/rooms/:email", async (req, res) => {
-			const result = await roomsCollection.find().toArray();
-			res.send(result);
-		});
+    app.get("/rooms/:email", async (req, res) => {
+      const result = await roomsCollection.find().toArray();
+      res.send(result);
+    });
 
-		// get specific meeting
-		app.get("/meetings/:email", async (req, res) => {
-			const email = req.params.email;
-			const result = await meetingsCollection.find({email: email}).toArray();
-			res.send(result);
-		});
+    // get specific meeting
+    app.get("/meetings/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await meetingsCollection.find({ email: email }).toArray();
+      res.send(result);
+    });
 
-		// save meeting
-		app.post("/meetings", async (req, res) => {
-			try {
-				const meetingData = req.body;
-				const result = await meetingsCollection.insertOne(meetingData);
+    // save meeting
+    app.post("/meetings", async (req, res) => {
+      try {
+        const meetingData = req.body;
+        const result = await meetingsCollection.insertOne(meetingData);
 
-				res.status(200).send("Meeting scheduled successfully");
-			} catch (error) {
-				console.error("Error scheduling meeting:", error);
-				res.status(500).send("An error occurred while scheduling the meeting");
-			}
-		});
+        res.status(200).send("Meeting scheduled successfully");
+      } catch (error) {
+        console.error("Error scheduling meeting:", error);
+        res.status(500).send("An error occurred while scheduling the meeting");
+      }
+    });
 
-		// delete meeting
-		app.delete("/meetings/:id", async (req, res) => {
-			const meetingId = req.params.id;
-			try {
-				const deleteResult = await meetingsCollection.deleteOne({
-					_id: new ObjectId(meetingId),
-				});
+    // delete meeting
+    app.delete("/meetings/:id", async (req, res) => {
+      const meetingId = req.params.id;
+      try {
+        const deleteResult = await meetingsCollection.deleteOne({
+          _id: new ObjectId(meetingId),
+        });
 
-				if (deleteResult.deletedCount === 1) {
-					res.status(200).send("Meeting deleted successfully");
-				} else {
-					res.status(404).send("Meeting not found");
-				}
-			} catch (error) {
-				console.error("Error deleting meeting:", error);
-				res.status(500).send("An error occurred while deleting the meeting");
-			}
-		});
+        if (deleteResult.deletedCount === 1) {
+          res.status(200).send("Meeting deleted successfully");
+        } else {
+          res.status(404).send("Meeting not found");
+        }
+      } catch (error) {
+        console.error("Error deleting meeting:", error);
+        res.status(500).send("An error occurred while deleting the meeting");
+      }
+    });
 
     app.get("/monthly", async (req, res) => {
       const result = await monthlyCololection.find().toArray();
@@ -241,14 +247,10 @@ async function run() {
     });
 
     // priceing id
-
     app.get("/monthly/:id", async (req, res) => {
       try {
-        const id = req.params.id; // Use req.params to access the URL parameter
+        const id = req.params.id;
 
-        console.log(id);
-        // Assuming you have a MongoDB collection named "monthlyCollection"
-        // Replace with your actual collection name
         const result = await monthlyCololection.findOne({
           _id: new ObjectId(id),
         });
@@ -265,44 +267,67 @@ async function run() {
     });
 
     app.get("/yearly/:id", async (req, res) => {
-      const id = req.body;
-      console.log(id);
-      const result = await yearlyCololection.find().toArray();
-      res.send(result);
+      try {
+        const id = req.params.id;
+        const result = await yearlyCololection.findOne({
+          _id: new ObjectId(id),
+        });
+
+        if (result) {
+          res.json(result);
+        } else {
+          res.status(404).json({ error: "Not found" });
+        }
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+      }
     });
 
+    // Payment Gateway SSLcommerz
     // order
-
     app.post("/order", async (req, res) => {
-      console.log(req.body.planId);
+      console.log(req.body);
 
-      const tranId = new ObjectId().toString();
+      const tran_id = new ObjectId().toString();
 
       const product = await monthlyCololection.findOne({
         _id: new ObjectId(req.body.planId),
       });
+      const productYearly = await yearlyCololection.findOne({
+        _id: new ObjectId(req.body.planId),
+      });
+
+      const products = product == null ? productYearly : product;
+
+      console.log(productYearly);
+
+      console.log("product", product);
 
       const data = {
-        total_amount: product?.price,
+        total_amount: parseFloat(
+          product?.price == null ? productYearly.price : product?.price
+        ),
         currency: "USD",
-        tran_id: tranId, // use unique tran_id for each api call
-        success_url: `http://localhost:5000/payment/success/${tranId}`,
-        fail_url: "http://localhost:3030/fail",
+        tran_id: tran_id, // use unique tran_id for each api call
+        success_url: `http://localhost:5000/payment/success/${tran_id}`,
+        fail_url: `http://localhost:5000/payment/faild/${tran_id}`,
         cancel_url: "http://localhost:3030/cancel",
         ipn_url: "http://localhost:3030/ipn",
         shipping_method: "Courier",
-        product_name: "Computer.",
+        product_name:
+          product?.title == null ? productYearly.title : product?.title,
         product_category: "Electronic",
         product_profile: "general",
-        cus_name: "Customer Name",
-        cus_email: "customer@example.com",
+        cus_name: req.body?.name,
+        cus_email: req.body?.email,
         cus_add1: "Dhaka",
         cus_add2: "Dhaka",
-        cus_city: "Dhaka",
+        cus_city: req.body?.city,
         cus_state: "Dhaka",
         cus_postcode: "1000",
-        cus_country: "Bangladesh",
-        cus_phone: "01711111111",
+        cus_country: req.body?.country,
+        cus_phone: req.body?.number,
         cus_fax: "01711111111",
         ship_name: "Customer Name",
         ship_add1: "Dhaka",
@@ -313,36 +338,75 @@ async function run() {
         ship_country: "Bangladesh",
       };
 
+      const {
+        total_amount,
+        currency,
+        product_name,
+        cus_name,
+        cus_email,
+        cus_city,
+        cus_country,
+        cus_phone,
+      } = data;
+      const Payment = {
+        total_amount,
+        currency,
+        product_name,
+        cus_name,
+        cus_email,
+        cus_city,
+        cus_country,
+        cus_phone,
+      };
+      console.log("d",Payment);
+
       console.log(data);
       const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
       sslcz.init(data).then((apiResponse) => {
+        // console.log(apiResponse);
         // Redirect the user to payment gateway
         let GatewayPageURL = apiResponse.GatewayPageURL;
         res.send({ url: GatewayPageURL });
+        const finalOrder = {
+          products,
+          paidStatus: false,
+          transactionId: tran_id,
+        };
+        const result = orderCololection.insertOne(finalOrder);
         console.log("Redirecting to: ", GatewayPageURL);
       });
 
-      const finalOrder = { product, paidStatus: false, transactionId: tranId };
-
-      const result = orderCololection.insertOne(finalOrder);
-
-      app.post("payment/success/:tranId", async (req, res) => {
-        console.log("tran", req.params.tranId);
-        const result = orderCololection.updateOne(
+      app.post("/payment/success/:tranId", async (req, res) => {
+        // console.log("tran", req.params.tranId);
+        const result = await orderCololection.updateOne(
           { transactionId: req.params.tranId },
           {
             $set: {
               paidStatus: true,
+              Payment,
             },
           }
         );
+
+        if (result.modifiedCount > 0) {
+          res.redirect(
+            `http://localhost:5173/payment/success/${req.params.tranId}`
+          );
+        }
       });
 
-      if (result.modifiedCount > 0) {
-        res.redirect(
-          `http://localhost:5173/payment/success/${req.params.tranId}`
-        );
-      }
+      app.post("/payment/faild/:tranId", async (req, res) => {
+        const result = await orderCololection.deleteOne({
+          transactionId: req.params.tranId,
+        });
+        if (result.deletedCount) {
+          res.redirect(
+            `http://localhost:5173/payment/faild/${req.params.tranId}`
+          );
+        }
+      });
+
+      // f
     });
 
     // Payment User Address
@@ -350,7 +414,7 @@ async function run() {
     app.post("/userAddress", async (req, res) => {
       try {
         const address = req.body;
-        console.log(address);
+        // console.log(address);
         const result = await userAddress.insertOne(address);
 
         res.status(201).json(result);
@@ -370,7 +434,46 @@ async function run() {
       }
     });
 
-    // Payment Gateway SSLcommerz
+    app.get("/paymentData/:tranId", async (req, res) => {
+      try {
+        const tranId = req.params.tranId;
+        const result = await orderCololection.findOne({
+          transactionId: tranId, // Match transactionId as a string
+        });
+
+        if (result) {
+          // Found a matching document
+          console.log("Found:", result);
+          res.status(200).json(result);
+        } else {
+          // No matching document found
+          console.log("No matching document found");
+          res.status(404).json({ message: "Payment data not found" });
+        }
+      } catch (error) {
+        // Handle any potential errors
+        console.error("Error:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    });
+
+    app.get("/payment/success/:tranId", async(req, res) => {
+          const id = req.params.tranId
+          const result = await orderCololection.findOne({ transactionId: id })
+          console.log(result);
+          res.send(result)
+          console.log(id);
+    })
+
+    app.get("/order", async (req, res) => {
+      // console.log(req.params.tranId);
+
+      // const tranId = req.params.tranId
+
+      const result = await orderCololection.find().toArray();
+      res.send(result);
+      // console.log(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
