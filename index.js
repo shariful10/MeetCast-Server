90badc2601c8c1be6bb8311799c3783251d801b6
Ahmed Nashif <a.nashif7@gmail.com>
2023-09-14 20:40:45 +0600

    jwt update

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
-const { generateToken04 } = require("./zegoServerAssistant");
+// const { generateToken04 } = require("./zegoServerAssistant");
 const socketServer = require("./Routes/sockets")
 
 //middleware
